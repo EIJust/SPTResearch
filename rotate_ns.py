86c259f90c6af8e38242ac8b2584bac926656c4f
Egor Isaev <e.isaev@di-group.biz>
2017-10-24 16:15:45 +0700

    remove quiet option from rotate_ns.py

--- rotate_ns.py
+++ rotate_ns.py
@@ -62,11 +62,7 @@ def rotate_positions(from_pos, to_pos):
 if __name__ == '__main__':
     parser = OptionParser()
 
-    parser.add_option("-f", "--file", dest="filename",
-                      help="write report to FILE", metavar="FILE")
-    parser.add_option("-q", "--quiet",
-                      action="store_false", dest="verbose", default=True,
-                      help="don't print status messages to stdout")
+    parser.add_option("-f", "--file", dest="filename")
 
     (options, args) = parser.parse_args()
 
